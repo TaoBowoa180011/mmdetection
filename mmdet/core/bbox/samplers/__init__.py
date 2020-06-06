3f108eb3e2c46ad64acaf8fcc18ba4b481f30ed8
TaoBowoa180011 <48510115+TaoBowoa180011@users.noreply.github.com>
2020-06-06 17:19:29 +0800

    Update __init__.py

--- mmdet/core/bbox/samplers/__init__.py
+++ mmdet/core/bbox/samplers/__init__.py
@@ -7,9 +7,10 @@ from .pseudo_sampler import PseudoSampler
 from .random_sampler import RandomSampler
 from .sampling_result import SamplingResult
 from .score_hlr_sampler import ScoreHLRSampler
+from .random_sampler_fixnum import RandomSamplerFixnum
 
 __all__ = [
     'BaseSampler', 'PseudoSampler', 'RandomSampler',
     'InstanceBalancedPosSampler', 'IoUBalancedNegSampler', 'CombinedSampler',
-    'OHEMSampler', 'SamplingResult', 'ScoreHLRSampler'
+    'OHEMSampler', 'SamplingResult', 'ScoreHLRSampler','RandomSamplerFixnum'
 ]
